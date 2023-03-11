b778efbc6c28eb3cb180bd178a99cd8114b03495
SanjeevThalod <97960636+SanjeevThalod@users.noreply.github.com>
2023-03-11 23:36:05 +0530

    Update 20.js

--- 20.js
+++ 20.js
@@ -14,3 +14,17 @@ body.addEventListener('mousemove',(event)=>{
         ele.remove();
     },2000)
 });
+body.addEventListener('click',(event)=>{
+    var x = event.offsetX;
+    var y = event.offsetY;
+    var ele = document.createElement('span');
+    var size = Math.random()*100;
+    ele.style.left = x + 'px';
+    ele.style.top = y + 'px';
+    ele.style.height = size + 'px';
+    ele.style.width = size + 'px';
+    body.appendChild(ele);
+    setTimeout(()=>{
+        ele.remove();
+    },2000)
+});
