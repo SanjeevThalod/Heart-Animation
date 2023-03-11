185dbacf89405961d80cae2585049ccfc388e533
SanjeevThalod <97960636+SanjeevThalod@users.noreply.github.com>
2023-03-11 23:47:41 +0530

    Update 20.js

--- 20.js
+++ 20.js
@@ -28,3 +28,17 @@ body.addEventListener('click',(event)=>{
         ele.remove();
     },2000)
 });
+body.addEventListener('touchmove',(event)=>{
+    var x = event.offsetX;
+    var y = event.offsetY;
+    var ele = document.createElement('span');
+    var size = Math.random()*100;
+    ele.style.left = x + 'px';
+    ele.style.top = y + 'px';
+    ele.style.height = size + 'px';
+    ele.style.width = size + 'px';
+    body.appendChild(ele);
+    setTimeout(()=>{
+        ele.remove();
+    },2000)
+});
